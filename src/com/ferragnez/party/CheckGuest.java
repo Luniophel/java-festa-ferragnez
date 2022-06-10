7ff47cfd41d1bdf4fba92840913ddb0ce3d5f1e2
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-10 17:54:21 +0200

    add dowhile cicle to checklist, fix wrong // on line 92 to 97

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -47,55 +47,59 @@ public class CheckGuest {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		//Questo ciclo si ripete se l'utente non conferma i dati inseriti
+		//Alla fine dell'esecuzione, l'utente può decidere se chiudere il programma o ripartire dall'inizio
 		do {
-			//Chiedi nome Utente
-			System.out.println("Inserisci le tue credenziali");
-			System.out.print("\r" + "Nome: ");
-			datiUtente = scan.nextLine();
-			
-			//Alcuni utenti non hanno cognome
-			System.out.println("(Lascia vuoto se non hai usato il cognome durante la registrazione)");
-			System.out.print("Cognome: ");
-			datiUtente = datiUtente + scan.nextLine(); //Qui verrà salvato il nome dell'utente
-			System.out.print("Confermi i dati inseriti?\nScrivi 'SI' per confermare: ");
-			confermo = scan.nextLine();
-			
-			//Chiedo se l'utente è sicuro dei dati inseriti
-			if (confermo.equalsIgnoreCase("SI")) { //Se risponde SI, esegue il controllo array
-			} else {
-				System.out.println("\nRisposta non valida. Riprova.\n");
-			} 
-		} while (!(confermo.equalsIgnoreCase("SI")));
+			//Questo ciclo si ripete se l'utente non conferma i dati inseriti
+			do {
+				//Chiedi nome Utente
+				System.out.println("Inserisci le tue credenziali");
+				System.out.print("\r" + "Nome: ");
+				datiUtente = scan.nextLine();
 
-		//Confronto con i nomi della lista
-		int counter = 0;
-		boolean check = false;
-		
-		//Versione base, con ciclo WHILE
-		while (check == false && counter < listaNomi.length) {
-			if(datiUtente.equalsIgnoreCase(listaNomi[counter])) {
-				check = true;
-			} 
-			else {
-				counter++;
+				//Alcuni utenti non hanno cognome
+				System.out.println("(Lascia vuoto se non hai usato il cognome durante la registrazione)");
+				System.out.print("Cognome: ");
+				datiUtente = datiUtente + scan.nextLine(); //Qui verrà salvato il nome dell'utente
+				System.out.print("Confermi i dati inseriti?\nScrivi 'SI' per confermare: ");
+				confermo = scan.nextLine();
+
+				//Chiedo se l'utente è sicuro dei dati inseriti
+				if (confermo.equalsIgnoreCase("SI")) { //Se risponde SI, esegue il controllo array
+				} else {
+					System.out.println("\nRisposta non valida. Riprova.\n");
+				}
+			} while (!(confermo.equalsIgnoreCase("SI")));
+			//Confronto con i nomi della lista
+			int counter = 0;
+			boolean check = false;
+			//Versione base, con ciclo WHILE
+			while (check == false && counter < listaNomi.length) {
+				if (datiUtente.equalsIgnoreCase(listaNomi[counter])) {
+					check = true;
+				} else {
+					counter++;
+				}
 			}
-		}
+			
+			//VERSIONE BONUS, con ciclo FOR
+//			for (counter = 0; counter < listaNomi.length; counter++ ) {
+//				if(datiUtente.equalsIgnoreCase(listaNomi[counter])) {
+//					check = true;
+//				} 
+//			}
+			
+			//Comunico se ho trovato il nome nella lista
+			if(check) {
+				System.out.println("Accesso consentito. Divertiti!");
+			}
+			else {
+				System.out.println("Accesso negato. Torna a casa!");
+			}
+			
+			System.out.println("\nVuoi chiudere la checklist? Scrivi 'CHIUDI' per confermare o\npremi INVIO per ricominciare:");
+			confermo = scan.nextLine();
+		} while (!(confermo.equalsIgnoreCase("CHIUDI")));
 		
-		//VERSIONE BONUS, con ciclo FOR
-//		for (counter = 0; counter < listaNomi.length; counter++ ) {
-//			if(datiUtente.equalsIgnoreCase(listaNomi[counter])) {
-//				check = true;
-//			} 
-//		}
-//		
-//		//Comunico se ho trovato il nome nella lista
-//		if(check) {
-//			System.out.println("Accesso consentito. Divertiti!");
-//		}
-//		else {
-//			System.out.println("Accesso negato. Torna a casa!");
-//		}
 		scan.close();
 
 	}
