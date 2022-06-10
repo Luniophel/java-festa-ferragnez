5e498a606c0d8f1118621b137aedcdd18a08a0a7
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-10 14:18:05 +0200

    first commit

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,27 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+	/*
+	 * Istruzioni:
+	 * 1. Crea e inizializza array con nomi invitati
+	 * 2. Chiedi nome all'utente
+	 * 3. Verifica se il nome è nell'array
+	 * 		Crea variabile booleana trovato = false
+	 * 		Controlla se nomeUtente corrisponde a nomeLista[i] per un numero di volte quanto array.length
+	 * 			Se non corrisponde, i++ e ripeti
+	 * 			Se corrisponde, trovato = true ed esci dal ciclo
+	 * 4. Se trovato, print "puoi entrare"
+	 * 5. Altrimenti print "Accesso negato"
+	 * 
+	 * Lista Invitati:
+	 * Dua Lipa, Paris Hilton, Manuel Agnelli,
+	 * J-Ax, Francesco Totti, Ilary Blasi,
+	 * Bebe Vio, Luis, Pardis Zarei,
+	 * Martina Maccherone, Rachel Zeilic
+	 */
+
+	public static void main(String[] args) {
+		
+	}
+
+}
